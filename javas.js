1858f57cb2dc53c7f67ca503b110e0dafe7e558a
gbrielchaves9 <gbrielchaves9@gmai.com>
2023-01-10 17:13:41 -0300

    layout ok

---
+++ javas.js
@@ -0,0 +1,14 @@
+const cards = document.querySelector("#pegacartas");
+const papagaios = [
+    'i1.gif',
+    'i2.gif',
+    'i3.gif',
+    'i4.gif',
+    'i5.gif',
+    'i6.gif',
+    'i7.gif'
+];
+
+papagaios.forEach(imagem =>{
+    cartas += ``
+})
