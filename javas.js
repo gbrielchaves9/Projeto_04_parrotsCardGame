bdd6ced1c3d66877077b1ba6d07d6d462bc3adf4
gbrielchaves9 <gbrielchaves9@gmai.com>
2023-01-16 08:18:16 -0300

    carta virando

--- javas.js
+++ javas.js
@@ -1,3 +1,50 @@
+let nCards = 0;
+
+w:
+while (nCards === 0) {
+  let ans = prompt("quantas?");
+
+  if (ans == null) continue;
+
+  let len = ans.length;
+
+  if (len > 2) continue;
+
+  for (let i = 0; i < len; i++) {
+    switch (ans[i]) {
+      case '0':
+        break;
+      case '1':
+        break;
+      case '2':
+        break;
+      case '3':
+        break;
+      case '4':
+        break;
+      case '5':
+        break;
+      case '6':
+        break;
+      case '7':
+        break;
+      case '8':
+        break;
+      case '9':
+        break;
+      default:
+        continue w;
+    };
+  };
+
+  if (ans < 4) continue;
+  if (ans > 14) continue;
+  if (ans % 2 !== 0) continue;
+
+  nCards = ans;
+};
+
+
 
 const cards = document.querySelector("#mesa");
 const papagaios = [
